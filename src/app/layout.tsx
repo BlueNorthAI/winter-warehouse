0e5dc548952ba34d72fa0b19a64fb6ac75536bb4
BlueNorthAI <github.bnai@gmail.com>
2025-03-20 15:54:32 +0530

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,8 @@ import { LicenseManager } from 'ag-grid-enterprise';
 const inter = Inter({ subsets: ["latin"] });
 
 export const experimental_ppr = true;
+export const runtime = 'edge';
+
 LicenseManager.setLicenseKey(
   '[TRIAL]_this_{AG_Charts_and_AG_Grid}_Enterprise_key_{AG-057528}_is_granted_for_evaluation_only___Use_in_production_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_purchasing_a_production_key_please_contact_info@ag-grid.com___You_are_granted_a_{Single_Application}_Developer_License_for_one_application_only___All_Front-End_JavaScript_developers_working_on_the_application_would_need_to_be_licensed___This_key_will_deactivate_on_{14 May 2024}____[v3]_[0102]_MTcxNTY0MTIwMDAwMA==6ff4143f8d6a412a9d66750abe4d9ae3'
 );
